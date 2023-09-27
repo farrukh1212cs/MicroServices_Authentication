c89396190080cfcefffac36a27a7f89f76105bce
Farrukh Rehman <50712521+farrukh1212cs@users.noreply.github.com>
2023-09-27 11:12:21 +0500

    Add project files.

---
+++ Client/IdentityController/ValidateController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Client.IdentityController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidateController : ControllerBase
+    {
+        private readonly SymmetricSecurityKey _key;
+        public ValidateController(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWTKey"]));
+        }
+        [HttpGet]
+        public bool ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = _key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
